327559807b22223ab6297db288a5f2ce3b4b89a5
hjb <hjbbjh0521@126.com>
2017-01-06 23:42:28 +0800

    add e

---
+++ Example/src/main/java/com/chinamobile/com/tbd/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.chinamobile.com.tbd;
+
+/**
+ * Created by hjb on 2017/1/6.
+ */
+public class HelloWorld {
+    private String a;
+    private String b;
+    private String c;
+    private String d;
+    private String e;
+    public HelloWorld() {
+    }
+}
---
+++ Example/src/main/java/com/chinamobile/com/tbd/Hellp.java
@@ -0,0 +1,9 @@
+package com.chinamobile.com.tbd;
+
+/**
+ * Created by hjb on 2017/1/6.
+ */
+public class Hellp {
+    public Hellp() {
+    }
+}
